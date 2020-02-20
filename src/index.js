2b1022e7f26e7f0e02fb8709f988df64eb56ff7d
dimitra86 <zliden01@protonmail.com>
2020-02-20 21:45:23 +0300

    321

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let arr = array.sort();
+    return arr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let result = array.reduce((sum, current) => (sum + current) / 2, 0);
+    return result;
 }
